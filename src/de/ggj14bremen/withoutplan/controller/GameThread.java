227549629855435f8687e4faa328ea153abc58c6
dragondagda <sliedtke57@gmail.com>
2014-01-25 15:40:13 +0100

    implemented init and spawn in GameThread.java

--- src/de/ggj14bremen/withoutplan/controller/GameThread.java
+++ src/de/ggj14bremen/withoutplan/controller/GameThread.java
@@ -1,9 +1,11 @@
 package de.ggj14bremen.withoutplan.controller;
 
+import android.util.Log;
 import de.ggj14bremen.withoutplan.GameSettings;
 import de.ggj14bremen.withoutplan.model.Board;
 import de.ggj14bremen.withoutplan.model.GameBoard;
 import de.ggj14bremen.withoutplan.model.GameState;
+import de.ggj14bremen.withoutplan.util.Generator;
 
 public class GameThread extends Thread implements Game{
 
@@ -13,7 +15,10 @@ public class GameThread extends Thread implements Game{
 	
 	private GameBoard board;
 	
+	private GameSettings settings;
+	
 	public GameThread(GameSettings gameSettings){
+		settings = gameSettings;
 		running = true;
 		board = new GameBoard();
 	}
@@ -25,6 +30,8 @@ public class GameThread extends Thread implements Game{
 			switch(this.state){
 			case INIT:
 				this.board.init();
+				final int amountEnemies = Generator.randomIntBetween(1, 3);
+				this.spawnEnemies(amountEnemies);
 				break;
 			case MOVE:
 				// TODO show move options
@@ -33,14 +40,36 @@ public class GameThread extends Thread implements Game{
 				// TODO show orientation options
 				break;
 			case ANALYSIS:
-				// TODO analyse board
+				this.analyseRound();
 				break;
 			case SPAWN:
-				
+				this.spawnEnemies(Generator.randomIntBetween(0, 2));
 				break;
 			case END:
 				// TODO show end monitor
 			}
+			
+			// TODO go into next state
+			
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+				Log.e("GameThread", e.getMessage());
+			}
+		}
+	}
+
+	private void analyseRound() {
+		// TODO analyse if enemy gets removed
+		// TODO check if game is over
+	}
+
+	private void spawnEnemies(int amountEnemies) {
+		for(int i=0; i<amountEnemies;i++){
+			final int x = Generator.randomIntBetween(0, this.settings.getBoardSizeX()-1);
+			final int y = Generator.randomIntBetween(0, this.settings.getBoardSizeY()-1);
+			// TODO check if cell has already an enemy
+			this.board.spawnEnemy(x, y);
 		}
 	}
 
---
+++ src/de/ggj14bremen/withoutplan/util/Generator.java
@@ -0,0 +1,9 @@
+package de.ggj14bremen.withoutplan.util;
+
+public class Generator {
+
+	
+	public static int randomIntBetween(int from, int to){
+		return (int) (Math.random() * (from - to) + to);
+	}
+}
