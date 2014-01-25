7e34e1f33a4fde16e91039241ef8a070320fa9d4
Boris <boris.walpert@enthusiastudios.com>
2014-01-25 15:09:05 +0100

    new base activity, basic gl rendering

---
+++ src/de/ggj14bremen/withoutplan/view/GLRenderer.java
@@ -0,0 +1,56 @@
+package de.ggj14bremen.withoutplan.view;
+
+
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLU;
+
+public class GLRenderer implements Renderer 
+{
+
+	private Square 		square;		// the square
+	
+	/** Constructor to set the handed over context */
+	public GLRenderer() {
+		this.square		= new Square();
+	}
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		// clear Screen and Depth Buffer
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+
+		// Reset the Modelview Matrix
+		gl.glLoadIdentity();
+
+		// Drawing
+		gl.glTranslatef(0.0f, 0.0f, -5.0f);		// move 5 units INTO the screen
+												// is the same as moving the camera 5 units away
+		square.draw(gl);						// Draw the triangle
+
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		if(height == 0) { 						//Prevent A Divide By Zero By
+			height = 1; 						//Making Height Equal One
+		}
+
+		gl.glViewport(0, 0, width, height); 	//Reset The Current Viewport
+		gl.glMatrixMode(GL10.GL_PROJECTION); 	//Select The Projection Matrix
+		gl.glLoadIdentity(); 					//Reset The Projection Matrix
+
+		//Calculate The Aspect Ratio Of The Window
+		GLU.gluPerspective(gl, 45.0f, (float)width / (float)height, 0.1f, 100.0f);
+
+		gl.glMatrixMode(GL10.GL_MODELVIEW); 	//Select The Modelview Matrix
+		gl.glLoadIdentity(); 					//Reset The Modelview Matrix
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+	}
+}
